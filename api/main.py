9c4fbbce22eda105a32c1ff12d7a985678b6e4c8
Omar EL FACHATI <elfachatiomar@gmail.com>
2023-11-23 21:01:27 +0100

    edit main

--- api/main.py
+++ api/main.py
@@ -1,7 +1,7 @@
 import uvicorn
 
 if __name__ == "__main__":
-    uvicorn.run("server.app:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("api.server.app:app", host="0.0.0.0", port=8000, reload=True)
 
 
 
