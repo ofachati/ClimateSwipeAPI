b578747a741761dc399c810cd5efdc4c058aeed3
Omar EL FACHATI <elfachatiomar@gmail.com>
2024-02-08 23:00:42 +0100

    add cros header

--- api/server/app.py
+++ api/server/app.py
@@ -2,10 +2,18 @@ from fastapi import FastAPI
 
 from api.server.routes.users import router as UsersRouter
 from api.server.routes.chatbot import router as chatbot_router
+from fastapi.middleware.cors import CORSMiddleware
 
 
 app = FastAPI()
-
+# Add CORS middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["https://itwi.me"],  # Adjust this to your Angular application URL
+    allow_credentials=True,
+    allow_methods=["GET", "POST", "PUT", "DELETE"],
+    allow_headers=["Authorization", "Content-Type"],
+)
 #app.include_router(UsersRouter, tags=["Users"], prefix="/user")
 
 app.include_router(chatbot_router, tags=["Ai"], prefix="/chat")
