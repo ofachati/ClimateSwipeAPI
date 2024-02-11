a5a1ac7e38e749ed6c9c55ce8821fd021cb87a1d
Omar EL FACHATI <elfachatiomar@gmail.com>
2024-02-11 21:00:57 +0100

    update swagger and error text

--- api/server/app.py
+++ api/server/app.py
@@ -1,11 +1,10 @@
 from fastapi import FastAPI
-
-from api.server.routes.users import router as UsersRouter
+from fastapi.openapi.utils import get_openapi
 from api.server.routes.chatbot import router as chatbot_router
 from fastapi.middleware.cors import CORSMiddleware
 
-
 app = FastAPI()
+
 # Add CORS middleware
 app.add_middleware(
     CORSMiddleware,
@@ -14,10 +13,25 @@ app.add_middleware(
     allow_methods=["GET", "POST", "PUT", "DELETE"],
     allow_headers=["Authorization", "Content-Type"],
 )
-#app.include_router(UsersRouter, tags=["Users"], prefix="/user")
 
-app.include_router(chatbot_router, tags=["Ai"], prefix="/chat")
+# Include your chatbot router
+app.include_router(chatbot_router, tags=["AI"], prefix="/chat")
+
+# Customized function to modify the OpenAPI schema
+def custom_openapi():
+    if app.openapi_schema:
+        return app.openapi_schema
+    openapi_schema = get_openapi(
+        title="ClimateSwipe API",  # Set the title to your project name
+        version="v0.2",  # Set the version to your desired version
+        description="API for ClimateSwipe",  # Describe your API here
+        routes=app.routes,
+    )
+    app.openapi_schema = openapi_schema
+    return app.openapi_schema
+
+app.openapi = custom_openapi
 
 @app.get("/", tags=["Root"])
 async def read_root():
-    return {"message": "Welcome to this fantastic app!"}
+    return {"message": "Welcome to the ClimateSwipe API!"}
